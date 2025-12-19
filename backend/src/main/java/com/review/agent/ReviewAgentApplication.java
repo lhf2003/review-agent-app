e4cae9ac00539ddee8886b8c40a4e568bc4f23b8
LHF <2385107101@qq.com>
2025-12-19 18:03:34 +0800

    feat: 添加@EnableSpringDataWebSupport注解

--- backend/src/main/java/com/review/agent/ReviewAgentApplication.java
+++ backend/src/main/java/com/review/agent/ReviewAgentApplication.java
@@ -3,10 +3,12 @@ package com.review.agent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @ServletComponentScan(basePackages = "com.review.agent.common.filter")
 public class ReviewAgentApplication {
 
